f28be7155b2d8f60375dab952602efd1d07fda73
Uchi8610 <b1900686@planet.kanazawa-it.ac.jp>
2019-07-31 08:35:07 +0900

    add master

---
+++ app.rb
@@ -0,0 +1,35 @@
+require 'sinatra'
+require 'sinatra/reloader'
+require 'sinatra/activerecord'
+
+require './models/user.rb'
+
+enable :method_override
+
+get '/' do
+  erb :index
+end
+
+get '/sign_up' do
+  erb :sign_up
+end
+
+post '/sign_up' do
+  sign_up_params = {
+    name: params[:name],
+    email: params[:email],
+    password: params[:password]
+  }
+  user = User.new(sign_up_params)
+  user.save
+  redirect 'users'
+end
+
+get '/users' do
+  @user = User.all
+  erb :users
+end
+
+configure do
+  set :server, :puma
+end
